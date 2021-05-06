fa29707990476f241576fd0dd67124b3443fc245
unmnn <uli.niemann@ovgu.de>
2021-05-06 16:52:34 +0200

    update block 6 slides after recording

--- slides/convert-html-to-pdf.R
+++ slides/convert-html-to-pdf.R
@@ -3,7 +3,7 @@ library(purrr)
 
 # dir("slides", pattern = "00.*html$") %>%
   # setdiff(c("01-R-Fundamentals.html", "04-rmd-intro.html")) %>%
-c("05-functions-iteration.html") %>%
+c("06-linear-models.html") %>%
   # Render html files from .Rmd
   # walk(~ paste0("slides/", xfun::sans_ext(.x), ".rmd"), "xaringan::moon_reader") %>%
   # Convert html files to pdf
